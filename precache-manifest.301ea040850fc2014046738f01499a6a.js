4910cc6017ede1e4fffaa6b0da783de78cd8384f
mmilic1994 <mmilic1994@gmail.com>
2019-07-02 01:01:43 +0200

    Updates

---
+++ precache-manifest.301ea040850fc2014046738f01499a6a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1d073f267ff0f5eb6d3889c4a7a6995",
+    "url": "/React-Calculator/index.html"
+  },
+  {
+    "revision": "ed26ca527988a30cc6f1",
+    "url": "/React-Calculator/static/css/main.7759fd80.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/React-Calculator/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "ed26ca527988a30cc6f1",
+    "url": "/React-Calculator/static/js/main.f0565129.chunk.js"
+  },
+  {
+    "revision": "19a8db08324e9b1cabe9",
+    "url": "/React-Calculator/static/js/runtime~main.cb778861.js"
+  }
+]);
